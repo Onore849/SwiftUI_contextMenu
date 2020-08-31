6bb10de9e2041a5dd5878e3c292b92a13401267a
Onore849 <rion3376@gmail.com>
2020-09-01 08:56:35 +0900

    first commit

--- contextMenu/ContentView.swift
+++ contextMenu/ContentView.swift
@@ -10,10 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        Image("pic").resizable().frame(height: 300)
+            .cornerRadius(20).padding()
+            .contextMenu{
+                
+                VStack {
+                    
+                    Button(action: {
+                        print("save")
+                    }) {
+                        
+                        HStack{
+                            
+                            Image(systemName: "folder.fill")
+                            Text("Save to Gallery")
+                        }
+                        
+                        
+                    }
+                    
+                    Button(action: {
+                        print("send")
+                    }) {
+                        
+                        HStack{
+                            
+                            Image(systemName: "paperplane.fill")
+                            Text("Send")
+                        }
+                        
+                }
+                
+        }
+        
     }
 }
 
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
